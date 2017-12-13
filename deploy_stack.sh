470b789d369fca3a5c8ef2bda83c299eeb10b5ee
d-smith <doug.smith.mail@gmail.com>
2017-12-13 15:13:17 -0800

    Added lambda skeleton

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 aws cloudformation create-stack --stack-name k2l \
+--parameters ParameterKey=CodeBucketName,ParameterValue=84101-demo-bucket \
 --template-body file://k2l.yml \
 --capabilities CAPABILITY_IAM
