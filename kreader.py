1d9db166eedc2992577aa1011f25a606833a56c7
d-smith <doug.smith.mail@gmail.com>
2017-12-14 20:06:13 -0800

    Added kinesis put to notebook, update function to extract and decode record data

--- kreader.py
+++ kreader.py
@@ -1,3 +1,8 @@
+import base64
+
 def lambda_handler(event, context):
-    print event
-    print context
+    records = event['Records']
+    for rec in records:
+        data = rec['kinesis']['data']
+        decoded = base64.b64decode(data)
+        print decoded
