b80f15d77cca7bafde671a5c8c0473737c110417
d-smith <doug.smith.mail@gmail.com>
2017-12-16 08:06:02 -0800

    Added invocation of lambda from stream processing lambda

--- kreader.py
+++ kreader.py
@@ -1,8 +1,25 @@
 import base64
+import boto3
+import os
+
+event_fn = os.environ['EVENT_LAMBDA_NAME']
+client = boto3.client('lambda')
+
 
 def lambda_handler(event, context):
+    
+    print event_fn
+    
     records = event['Records']
     for rec in records:
         data = rec['kinesis']['data']
         decoded = base64.b64decode(data)
         print decoded
+        
+        response = client.invoke(
+            FunctionName=event_fn,
+            InvocationType='Event',
+            Payload=decoded
+        )
+
+        print response
